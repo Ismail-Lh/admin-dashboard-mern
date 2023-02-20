7b5cfec0c5f13c3278d95434e0455872358a6d77
Ismail-Lh <ismailsaad03@gmail.com>
2023-02-20 17:34:41 +0100

    create basic backend API routes

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,11 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 
+import clientRoutes from './routes/clientRoutes.js';
+import generalRoutes from './routes/generalRoutes.js';
+import managementRoutes from './routes/managementRoutes.js';
+import salesRoutes from './routes/salesRoutes.js';
+
 // CONFIGURATION
 dotenv.config();
 const app = express();
@@ -16,3 +21,9 @@ app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
+// ROUTES
+app.use('/api/client', clientRoutes);
+app.use('/api/general', generalRoutes);
+app.use('/api/management', managementRoutes);
+app.use('/api/sales', salesRoutes);
