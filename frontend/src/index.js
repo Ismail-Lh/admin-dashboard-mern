50dcface465b0634ec30f4136c1cbdebb29cc533
Ismail-Lh <ismailsaad03@gmail.com>
2023-02-21 16:06:39 +0100

    Add react-redux initial configuration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+import globalReducer from 'state';
+
 import './index.css';
 import App from './App';
+
+const store = configureStore({
+  global: globalReducer,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
