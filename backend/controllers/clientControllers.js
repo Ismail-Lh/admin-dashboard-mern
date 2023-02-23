1837b69a72d4e0a752c0ce1ed0ae7c47e31ce214
Ismail-Lh <ismailsaad03@gmail.com>
2023-02-23 18:14:11 +0100

    create getTransactions backend functionality with sort, search && pagination

--- backend/controllers/clientControllers.js
+++ backend/controllers/clientControllers.js
@@ -2,8 +2,8 @@ import asyncHandler from 'express-async-handler';
 
 import Product from '../models/productModel.js';
 import ProductState from '../models/productStateModel.js';
-
 import User from '../models/userModel.js';
+import Transaction from '../models/transactionModel.js';
 
 export const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find();
@@ -23,6 +23,42 @@ export const getCustomers = asyncHandler(async (req, res) => {
   const customers = await User.find({ role: 'user' }).select('-password');
 
   res.status(200).json(customers);
+});
+
+// *getTransactions functionality with sort, search && pagination
+export const getTransactions = asyncHandler(async (req, res) => {
+  // !sort should lok like {"field": "userId", "sort": "desc"}
+  const { page = 1, pageSize = 20, sort = null, search = '' } = req.query;
+
+  // !formatted sort should lok like {userId: -1}
+  const generateSort = () => {
+    const sortParsed = JSON.parse(sort);
+
+    const sortFormatted = {
+      [sortParsed.field]: (sortParsed.sort = 'asc' ? 1 : -1),
+    };
+
+    return sortFormatted;
+  };
+
+  const sortFormatted = Boolean(sort) ? generateSort() : {};
+
+  const transactions = await Transaction.find({
+    $or: [
+      { cost: { $regex: new RegExp(search, 'i') } },
+      { userId: { $regex: new RegExp(search, 'i') } },
+    ],
+  })
+    .sort(sortFormatted)
+    .skip(page * pageSize)
+    .limit(pageSize);
+
+  // *get the totalTransactions number
+  const totalTransactionsNum = await Transaction.countDocuments({
+    name: { $regex: search, $options: 'i' },
+  });
+
+  res.status(200).json({ transactions, totalTransactionsNum });
 });
 
 // export const getProductsWithState = asyncHandler(async (req, res) => {
