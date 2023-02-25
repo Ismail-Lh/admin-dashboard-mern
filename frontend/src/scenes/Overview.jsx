7333c59c770d083cac96c2b60222797c011c20d2
Ismail-Lh <ismailsaad03@gmail.com>
2023-02-25 16:47:29 +0100

    Create the Overview page with the ResponsiveLine chart

--- frontend/src/scenes/Overview.jsx
+++ frontend/src/scenes/Overview.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
-import { useGetOverallSalesQuery } from 'state/api';
+import { useState } from 'react';
+import { FormControl, MenuItem, InputLabel, Box, Select } from '@mui/material';
+
+import Header from 'components/Header';
+import OverviewChart from 'components/OverviewChart';
 
 const Overview = () => {
-  const { data } = useGetOverallSalesQuery();
-  console.log('🚀 ~ file: Overview.jsx:6 ~ Overview ~ data:', data);
+  const [view, setView] = useState('units');
 
-  return <div>Overview</div>;
+  return (
+    <Box m='1.5rem 2.5rem'>
+      <Header
+        title='OVERVIEW'
+        subtitle='Overview of general revenue and profit'
+      />
+      <Box height='75vh'>
+        <FormControl sx={{ mt: '1rem' }}>
+          <InputLabel>View</InputLabel>
+          <Select
+            value={view}
+            label='View'
+            onChange={e => setView(e.target.value)}>
+            <MenuItem value='sales'>Sales</MenuItem>
+            <MenuItem value='units'>Units</MenuItem>
+          </Select>
+        </FormControl>
+        <OverviewChart view={view} />
+      </Box>
+    </Box>
+  );
 };
 
 export default Overview;
