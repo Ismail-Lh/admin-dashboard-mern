190982bfad903d58f119dae8ceaa78af832c40b8
Ismail-Lh <ismailsaad03@gmail.com>
2023-02-27 18:17:26 +0100

    Create the getUserPerformance sales backend functionality

--- backend/controllers/managementControllers.js
+++ backend/controllers/managementControllers.js
@@ -1,6 +1,9 @@
+import mongoose from 'mongoose';
 import asyncHandler from 'express-async-handler';
 
 import User from '../models/userModel.js';
+import AffiliateStat from '../models/affiliateStatModel.js';
+import Transaction from '../models/transactionModel.js';
 
 // *@desc Get all admins
 // *@route GET /api/management/admins
@@ -9,4 +12,37 @@ export const getAdmins = asyncHandler(async (req, res) => {
   const admins = await User.find({ role: 'admin' }).select('-password');
 
   res.status(200).json(admins);
+});
+
+// *@desc Get user sales performance
+// *@route GET /api/management/performance/:id
+// *@access public (for now)
+export const getUserPerformance = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  const userWithStats = await User.aggregate([
+    { $match: { _id: new mongoose.Types.ObjectId(id) } },
+    {
+      $lookup: {
+        from: 'affiliatestats',
+        localField: '_id',
+        foreignField: 'userId',
+        as: 'affiliateStats',
+      },
+    },
+    { $unwind: '$affiliateStats' },
+  ]);
+
+  const saleTransactions = await Promise.all(
+    userWithStats[0].affiliateStats.affiliateSales.map(id => {
+      return Transaction.findById(id);
+    })
+  );
+  const filteredSaleTransactions = saleTransactions.filter(
+    transaction => transaction !== null
+  );
+
+  res
+    .status(200)
+    .json({ user: userWithStats[0], sales: filteredSaleTransactions });
 });
--- backend/routes/managementRoutes.js
+++ backend/routes/managementRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express';
 
-import { getAdmins } from '../controllers/managementControllers.js';
+import {
+  getAdmins,
+  getUserPerformance,
+} from '../controllers/managementControllers.js';
 
 const router = express.Router();
 
 router.route('/admins').get(getAdmins);
+router.route('/performance/:id').get(getUserPerformance);
 
 export default router;
