bfacfd0e04d0c6b43c32b9cb0d6b5651de0aea23
Ismail-Lh <ismailsaad03@gmail.com>
2023-02-27 16:22:41 +0100

    Create the Monthly sales chart page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import Transactions from 'scenes/Transactions';
 import Geography from 'scenes/Geography';
 import Overview from 'scenes/Overview';
 import Daily from 'scenes/Daily';
+import Monthly from 'scenes/Monthly';
 
 function App() {
   const mode = useSelector(state => state.global.mode);
@@ -33,6 +34,7 @@ function App() {
             <Route path='/geography' element={<Geography />} />
             <Route path='/overview' element={<Overview />} />
             <Route path='/daily' element={<Daily />} />
+            <Route path='/monthly' element={<Monthly />} />
           </Route>
         </Routes>
       </ThemeProvider>
