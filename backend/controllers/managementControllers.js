4dbc32bf42f527e7da24ca8450896bd90c4f3a84
Ismail-Lh <ismailsaad03@gmail.com>
2023-02-27 16:55:53 +0100

    Create the getAdmins backend functionality

---
+++ backend/controllers/managementControllers.js
@@ -0,0 +1,10 @@
+import User from '../models/userModel.js';
+
+// *@desc Get all admins
+// *@route GET /api/management/admins
+// *@access public (for now)
+export const getAdmins = asyncHandler(async (req, res) => {
+  const admins = await User.find({ role: 'admin' }).select('-password');
+
+  res.status(200).json(admins);
+});
--- backend/routes/managementRoutes.js
+++ backend/routes/managementRoutes.js
@@ -1,5 +1,9 @@
 import express from 'express';
 
+import { getAdmins } from '../controllers/managementControllers';
+
 const router = express.Router();
+
+router.route('/admins').get(getAdmins);
 
 export default router;
